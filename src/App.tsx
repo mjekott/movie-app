caa65ecdcadd6907b0b41481eb0970febec47e72
mfonobong ekott <ekottmfon@yahoo.com>
2022-03-16 20:05:25 +0100

    features/added folder structure and implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from 'components/Navigation';
+import Home from 'pages/Home';
+import Starred from 'pages/Starred';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/starred" element={<Starred />} />
+      </Routes>
     </div>
   );
 }
