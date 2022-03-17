2abe82baebf32f3d0592792c328033f900930091
mfonobong ekott <ekottmfon@yahoo.com>
2022-03-17 10:21:13 +0100

    style:installed styled components and creates styles for seacrhcards

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
