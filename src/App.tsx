9e3e8265cb0fc2779fe1a974e288872bf177956e
mfonobong ekott <ekottmfon@yahoo.com>
2022-03-17 10:29:47 +0100

    feature:show page implementaion

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from 'components/Navigation';
 import Home from 'pages/Home';
 import Starred from 'pages/Starred';
+import Show from 'pages/Show';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/show/:id" element={<Show />} />
         <Route path="/starred" element={<Starred />} />
       </Routes>
     </div>
